833f62c58d327f95390f07e3c74785cdd40fc358
Mafpo <mafpo@seznam.cz>
2025-06-29 18:41:27 +0200

    Add initial method definitions for ID3Reader

---
+++ src/ID3Reader.cpp
@@ -0,0 +1,13 @@
+#include "ID3Reader.hpp"
+
+ID3Reader::ID3Reader(const std::string & filePath){
+    
+}
+
+std::string ID3Reader::getTitle() const{
+
+}
+
+bool ID3Reader::extractPicture() const{
+
+}
